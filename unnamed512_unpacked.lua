2f384d657f6474d3a59c8a31347d0da38d4062d9
Veikko Sariola <5684185+vsariola@users.noreply.github.com>
2021-12-07 22:28:34 +0200

    Improve visuals and add debug.

--- unnamed512_unpacked.lua
+++ unnamed512_unpacked.lua
@@ -1,4 +1,5 @@
 d={
+ 0,0,0,0,
  1,2,2,2,1,2,2,0, --chn 0 bass
  0,1,3,3,3,3,3,0, --chn 1 mid
  5,5,5,5,0,5,5,5, --chn 2 snare
@@ -21,9 +22,10 @@ function TIC()
   p=t//1024
   poke(65896,48)
   e=t*(2-k//2)
-  a=d[8*k+p+1]+t//128%8//7
-  n=d[8*a+e//16%8+17]
+  a=d[8*k+p+5]+t//128%8//7
+  n=d[8*a+e//16%8+21]
   d[-k]=-e%16%(a//2*16*n+1)
+  d[k+1]=d[k+1]+d[-k]
   sfx(
    k%3,
    3
@@ -38,8 +40,8 @@ function TIC()
   )
  end
  t=t+1,t<8192 or exit()
- c=c+d[-2]
- f=f+d[0]
+ c=d[3]
+ f=d[1]
  cls(c%9*4)
 
  n=t//1024
@@ -48,27 +50,51 @@ function TIC()
  for k=0,1 do
   for i=0,2400 do 
    z=1-i/1200
-   r=(1-z*z)^.5
+   r=(1-z*z)^.5   
    w=m*i
    x=math.cos(w)*r
    y=math.sin(w)*r
-   r=s(s(x*5+c/7+f/29)
+   if p//2%2==1 then
+    y=y//.4*.4+.2
+   end
+   r=s(s(x*5+c/7+f/40)
     +s(y*5+c/8)
     +s(z*5+c/9)
     +q/3)^(c%7+1)*(s(c*(71+p))^2*2)/4+1
    z=z*r+2.1
    h=p//5*d[-3]/2
-   d[p//3%2+89](
+   d[p//3%2+93](
     x*r*99/z+120-6/z+h/2,
     (y*r*k+1-k)*99/z+68-6/z+h/2,
     12/z-h,
     12/z-h,
-    k*(-d[-3]*z+4*y*(.5-c%7%2)*d[-1])/3
+    k*(-d[-3]/(.3+z)*2-s(x/.2-t/64*3.14)^5*d[-1])/3
    )
   end
  end 
+ debug()
 end
 s=math.sin
+
+function debug()
+ updatetime = false
+ if btn(2) and t>100 then t=t-100 updatetime=true end
+ if btn(3) then t=t+100 updatetime=true end
+ if updatetime then
+   for k=1,4 do d[k]=0 end   
+   for i=0,t-1 do
+    for k=0,3 do
+     p=i//1024
+     e=i*(2-k//2)
+     a=d[8*k+p+5]+i//128%8//7
+     n=d[8*a+e//16%8+21]
+     d[-k]=-e%16%(a//2*16*n+1)   
+     d[k+1]=d[k+1]+d[-k]
+    end
+   end
+ end
+end
+
 -- <TILES>
 -- 001:eccccccccc888888caaaaaaaca888888cacccccccacc0ccccacc0ccccacc0ccc
 -- 002:ccccceee8888cceeaaaa0cee888a0ceeccca0ccc0cca0c0c0cca0c0c0cca0c0c
